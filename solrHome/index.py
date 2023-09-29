b0cb6e9be50f26cb6301c79082f7ca57bd47ee43
Anna Ruggero <a.ruggero@sease.io>
2023-09-29 12:01:31 +0200

    Update indexing code

--- solrHome/index.py
+++ solrHome/index.py
@@ -1,23 +1,22 @@
 import pysolr
 import json
 
-def books():
+
+if __name__ == "__main__":
+    solr = pysolr.Solr('http://localhost:8983/solr/books', timeout=100)
+
     books = json.loads(open('books.json').read())
+    documents = []
+
     for book in books:
         id = book['metadata']['id']
         print("Indexing %s" % id)
-        try:
-            yield {'id': id,
+        doc = {'id': id,
                    'title': book['bibliography']['title'],
                    'author': book['bibliography']['author']['name'],
                    'downloads': book['metadata']['downloads'],
                    'languages': [language for language in book['bibliography']['languages']],
                    'subjects': [subject for subject in book['bibliography']['subjects']]}
-        except KeyError as k:
-            print(k)
-            continue
-
-
-if __name__ == "__main__":
-    solr = pysolr.Solr('http://localhost:8983/solr/books', timeout=100)
-    solr.add(books())
+        documents.append(doc)
+    solr.add(documents)
+    print("finished")
